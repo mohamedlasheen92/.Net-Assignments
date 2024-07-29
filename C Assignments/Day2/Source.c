37793fec55f98616f0b1ae8c9f4820598f16aee1
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-07-29 23:43:16 +0300

    C Day2 Assignment P1

---
+++ C Assignments/Day2/Source.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+#pragma region A Program to ask userfor integer, and check if it is even or odd
+	/*int num;
+	printf("Enter a Number: ");
+	scanf("%i", &num);
+	if (num % 2 == 0)
+		printf("Even");
+	else
+		printf("Odd");*/
+#pragma endregion
+
+#pragma region A Program to ask user for a degree and print the equivilant grade
+	/*char degree;
+	printf("Enter your degree: ");
+	degree = getchar();
+
+	switch (degree) {
+	case 'A':
+		printf("Excellent\n");
+		break;
+	case 'B':
+		printf("Very Good\n");
+		break;
+	case 'C':
+		printf("Good\n");
+		break;
+	case 'D':
+		printf("Fair\n");
+		break;
+	case 'E':
+		printf("Failed\n");
+		break;
+	default:
+		printf("No Match\n");
+
+	}*/
+
+#pragma endregion
+
+
+
+
+
+	return 0;
+}
