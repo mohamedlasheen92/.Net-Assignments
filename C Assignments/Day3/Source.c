62d8ac7dc203fd0d85a3596f1b24fca7680eb5ff
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-08-01 13:42:53 +0300

    C Day3 Assignment P2

--- C Assignments/Day3/Source.c
+++ C Assignments/Day3/Source.c
@@ -1,16 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 
 #pragma region A Program that Gets the scores from user of 5 Subjects for 1 student,and display them on the screen
+	/*
+	* Get Max,Min Grades (bonus)
+	* Merge with Simple Menu (bonus)
+	*/
+	/*
 	int grades[5] = { 0, 0, 0, 0, 0 };
 	int maxGrade = 0, minGrade = 101;
 	char choice;
 
 	do {
-
 
 		printf("Press 'a' to Add All.\n");
 		printf("Press 'e' to Edit.\n");
@@ -26,7 +29,7 @@ int main() {
 				scanf_s("%i", &grades[i]);
 
 				if (grades[i] >= maxGrade) maxGrade = grades[i];
-				else if (grades[i] < minGrade) minGrade = grades[i];
+				if (grades[i] < minGrade) minGrade = grades[i];
 
 			}
 			break;
@@ -53,6 +56,38 @@ int main() {
 		printf("***********************\n");
 
 	} while (choice != 'x');
+	*/
+#pragma endregion
+
+#pragma region A Program that gets from user the scores of 4 students in 5 subjects,and display then on the screen,
+	/*
+	* Calculate Sum for each Student
+	* Calculate Average for each Subject
+	*/
+
+	int scores[4][5];
+	int studentsGradesSum[4] = { 0, 0, 0, 0 };
+	int subjectAverage[5] = { 0, 0, 0, 0, 0 };
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("Enter the Score of Subject %i for Student %i: ", j + 1, i + 1);
+			scanf_s("%i", &scores[i][j]);
+			studentsGradesSum[i] += scores[i][j];
+			subjectAverage[j] += scores[i][j];
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("The Score of Subject %i for Student %i: %i \n", j + 1, i + 1, scores[i][j]);
+		}
+		printf("The Sum of Grades for Student %i: %i \n", i + 1, studentsGradesSum[i]);
+	}
+	for (int i = 0; i < 5; i++) {
+		float average = (float)subjectAverage[i] / 4;
+		printf("The Average Grades of Subject %i: %f \n", i + 1, average);
+	}
+
 
 
 
